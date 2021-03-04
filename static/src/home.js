fbaf427c5147cbd2b560e826c416cd3d8e84adff
Supriya Mutharasan <smutharasan@myseneca.ca>
2021-03-03 20:59:05 -0500

    video works, peer module added, profileInfoView added

--- static/src/home.js
+++ static/src/home.js
@@ -4,47 +4,47 @@
  * Date: 1/3/2021
  ********************************/
 // Initialize DOM selectors
-let homeHeader = document.querySelector(".header");
-let homehamburgerMenu = document.querySelector(".hamburgerMenu");
+let homeHeader = document.querySelector('.header');
+let homehamburgerMenu = document.querySelector('.hamburgerMenu');
 
 // PC View only
-window.addEventListener("scroll", function () {
-  let wPOS = window.scrollY > 70;
-  homeHeader.classList.toggle("active", wPOS);
-});
+// window.addEventListener("scroll", function () {
+//   let wPOS = window.scrollY > 70;
+//   homeHeader.classList.toggle("active", wPOS);
+// });
 // Mobile View only
-homehamburgerMenu.addEventListener("click", function () {
-  homeHeader.classList.toggle("menuToggled");
+homehamburgerMenu.addEventListener('click', function () {
+  homeHeader.classList.toggle('menuToggled');
 });
 
 document
-  .getElementById("signInForm")
-  .addEventListener("submit", signInFormValidate);
+  .getElementById('signInForm')
+  .addEventListener('submit', signInFormValidate);
 
 function signInFormValidate(givenEvent) {
   const passwordExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
 
   var validationFlag = true;
 
-  var signInFormUsername = document.getElementById("signInFormUsername");
-  var tester = document.getElementsByName("signInFormPassword");
+  var signInFormUsername = document.getElementById('signInFormUsername');
+  var tester = document.getElementsByName('signInFormPassword');
   console.log(tester);
-  var signInFormPassword = document.getElementById("signInFormPassword");
+  var signInFormPassword = document.getElementById('signInFormPassword');
 
   refreshModal(signInFormUsername, signInFormPassword);
 
-  if (signInFormUsername.value === "") {
-    signInFormUsername.classList.add("is-invalid");
+  if (signInFormUsername.value === '') {
+    signInFormUsername.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signInFormUsername.classList.add("is-valid");
+    signInFormUsername.classList.add('is-valid');
   }
 
   if (!signInFormPassword.value.match(passwordExp)) {
-    signInFormPassword.classList.add("is-invalid");
+    signInFormPassword.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signInFormPassword.classList.add("is-valid");
+    signInFormPassword.classList.add('is-valid');
   }
 
   if (!validationFlag) {
@@ -53,19 +53,19 @@ function signInFormValidate(givenEvent) {
 }
 
 document
-  .getElementById("signUpForm")
-  .addEventListener("submit", signUpFormValidate);
+  .getElementById('signUpForm')
+  .addEventListener('submit', signUpFormValidate);
 
 function signUpFormValidate(givenEvent) {
   /*Minimum ten characters, at least one uppercase letter, one lowercase letter, one number and one special character: */
   const passwordExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
   var validationFlag = true;
 
-  var signUpFormFirstName = document.getElementById("signUpFormFirstName");
-  var signUpFormLastName = document.getElementById("signUpFormLastName");
-  var signUpFormUsername = document.getElementById("signUpFormUsername");
-  var signUpFormEmail = document.getElementById("signUpFormEmail");
-  var signUpFormPassword = document.getElementById("signUpFormPassword");
+  var signUpFormFirstName = document.getElementById('signUpFormFirstName');
+  var signUpFormLastName = document.getElementById('signUpFormLastName');
+  var signUpFormUsername = document.getElementById('signUpFormUsername');
+  var signUpFormEmail = document.getElementById('signUpFormEmail');
+  var signUpFormPassword = document.getElementById('signUpFormPassword');
 
   refreshModal(
     signUpFormFirstName,
@@ -75,39 +75,39 @@ function signUpFormValidate(givenEvent) {
     signUpFormPassword
   );
 
-  if (signUpFormFirstName.value === "") {
-    signUpFormFirstName.classList.add("is-invalid");
+  if (signUpFormFirstName.value === '') {
+    signUpFormFirstName.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signUpFormFirstName.classList.add("is-valid");
+    signUpFormFirstName.classList.add('is-valid');
   }
 
-  if (signUpFormLastName.value === "") {
-    signUpFormLastName.classList.add("is-invalid");
+  if (signUpFormLastName.value === '') {
+    signUpFormLastName.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signUpFormLastName.classList.add("is-valid");
+    signUpFormLastName.classList.add('is-valid');
   }
 
-  if (signUpFormUsername.value === "") {
-    signUpFormUsername.classList.add("is-invalid");
+  if (signUpFormUsername.value === '') {
+    signUpFormUsername.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signUpFormUsername.classList.add("is-valid");
+    signUpFormUsername.classList.add('is-valid');
   }
 
-  if (signUpFormEmail.value === "") {
-    signUpFormEmail.classList.add("is-invalid");
+  if (signUpFormEmail.value === '') {
+    signUpFormEmail.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signUpFormEmail.classList.add("is-valid");
+    signUpFormEmail.classList.add('is-valid');
   }
 
   if (!signUpFormPassword.value.match(passwordExp)) {
-    signUpFormPassword.classList.add("is-invalid");
+    signUpFormPassword.classList.add('is-invalid');
     validationFlag = false;
   } else {
-    signUpFormPassword.classList.add("is-valid");
+    signUpFormPassword.classList.add('is-valid');
   }
 
   if (!validationFlag) {
@@ -117,7 +117,7 @@ function signUpFormValidate(givenEvent) {
 
 function refreshModal(...givenArgs) {
   givenArgs.forEach((element) => {
-    element.classList.remove("is-invalid");
-    element.classList.remove("is-valid");
+    element.classList.remove('is-invalid');
+    element.classList.remove('is-valid');
   });
 }
--- static/src/roomScript.js
+++ static/src/roomScript.js
@@ -12,15 +12,23 @@ navigator.mediaDevices
   .then((stream) => {
     console.log('hello');
     addVideoStream(myVideo, stream);
-  });
+    myPeer.on('call', (call) => {
+      console.log('CAlling the other person');
+      call.answer(stream);
+    });
+    socket.on('user-connected', (givenUserID) => {
+      connectToNewUser(givenUserID, stream);
+    });
+  })
+  .catch((err) => console.error(err));
 
 myPeer.on('opem', (id) => {
   socket.emit('join-room', ROOM_ID, id);
 });
 
-socket.on('user-connected', (givenUserID) => {
-  console.log('hello + ' + givenUserID);
-});
+// socket.on('user-connected', (givenUserID) => {
+//   console.log('hello + ' + givenUserID);
+// });
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
@@ -29,4 +37,15 @@ function addVideoStream(video, stream) {
     video.play();
   });
   videoGrid.append(video);
+}
+
+function connectToNewUser(givenID, givenStream) {
+  const call = myPeer.call(givenID, givenStream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on('close', () => {
+    video.remove();
+  });
 }
